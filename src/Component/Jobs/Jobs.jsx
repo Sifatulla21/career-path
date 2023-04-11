a90c52e48e0eb427f4d386c7fdecab9febcf41f4
Sifatulla21 <sifat2109@gmail.com>
2023-04-11 15:53:05 +0600

    sort button done

--- src/Component/Jobs/Jobs.jsx
+++ src/Component/Jobs/Jobs.jsx
@@ -1,34 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import JobCard from './JobCard';
 import './Jobs.css'
 const Jobs = () => {
     const JobData = useLoaderData();
-    const sortedRemote = JobData.filter(job => job.jPlace === "Remote") ;
+    const [sort, setSort] = useState();
+    const [sortRemoteBtn, setSortRemoteBtn] = useState(false);
+    const [sortOnSiteBtn, setSortOnSiteBtn] = useState(false);
     const sortRemote = () => {
         const sortedRemote = JobData.filter(job => job.jPlace === "Remote");
-            if(sortedRemote){
-                console.log(sortedRemote);
-            }
+        setSort(sortedRemote);
+        setSortRemoteBtn(true);  
+    }
+    const sortOnSite = () => {
+        const sortedOnSite = JobData.filter(job => job.jPlace === "Onsite");
+        setSort(sortedOnSite);
+        setSortOnSiteBtn(true);  
     }
     return (
         <div className="job-card-container">
             <h1>Applied Jobs</h1>
             <div className="sort-btn-container">
-                <button onClick={() => sortRemote()}>Sort By Remote</button>
-                <button>Sort By On Site</button>
+             {
+              <button onClick={() => sortRemote()}>Sort By Remote</button> 
+             }     
+            {
+              <button onClick={() => sortOnSite()}>Sort By On Site</button>
+                }
             </div>
             {
-                JobData.map(data => <JobCard
+                sort ? sort.map(data => <JobCard
                     key={data.id}
                     data={data}
-                ></JobCard>)
+                ></JobCard>)  : JobData.map(data => <JobCard
+                    key={data.id}
+                    data={data}
+                ></JobCard>) 
             }
-            {/* {
-                sortedRemote.map(remote => <JobCard
-                    remote={remote}
-                ></JobCard>)
-            } */}
+            
         </div>
     );
 };
